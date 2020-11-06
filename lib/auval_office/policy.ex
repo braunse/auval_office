6a7f241b02367ad403ddf65226d39fbacc852325
Sebastien Braun <sebastien@sebbraun.de>
2020-11-06 22:19:13 +0100

    Add documentation

--- lib/auval_office/policy.ex
+++ lib/auval_office/policy.ex
@@ -17,11 +17,16 @@ defmodule AuvalOffice.Policy do
   See the high-level docs in `AuvalOffice` to understand what's going on.
   """
 
+  @type authorization_result() ::
+    {:ok, :atom, Keyword.t()} |
+    {:error, :atom, Keyword.t()}
+
   defmacro __using__(_options \\ []) do
     quote do
       import unquote(__MODULE__)
       Module.register_attribute(__MODULE__, :rules, accumulate: true)
       Module.register_attribute(__MODULE__, :fetchers, accumulate: true)
+      @behaviour unquote(__MODULE__)
       @before_compile unquote(__MODULE__)
     end
   end
@@ -67,9 +72,30 @@ defmodule AuvalOffice.Policy do
 
   @doc """
   Define a fetcher.
+
+  Takes an additional `do` block, or a `do:` expression.
+
+  ### Parameters
+
+  * `id`: An identifier to disambiguate this fetcher
+  * `attr`: The attribute that will be set in the context.
+    The fetcher will be skipped if there is already an attribute by this name in the context.
+
+  ### Options
+
+  * `subject`: A pattern to match against the subject being authorized
+  * `object`: A pattern to match against the object being authorized
+  * `action`: A pattern to match against the action being authorized
+  * `context`: A pattern to match against the context built up so far
+  * `when`: A guard. The fetcher will be skipped if this evaluates to false.
+    Can refer to variables bound in the matchers.
+    Will be evaluated in the context of an `if` block, so not limited to guard expressions.
   """
   defmacro fetch(id, attr, options), do: do_fetch(id, attr, options)
-  defmacro fetch(id, attr, options, do: fetch_block), do: do_fetch(id, attr, [do: fetch_block] ++ options)
+
+  @doc false
+  defmacro fetch(id, attr, options, do: fetch_block),
+    do: do_fetch(id, attr, [do: fetch_block] ++ options)
 
   defp do_rule(id, actions, subject, object, options) do
     rule_block = Keyword.fetch!(options, :do)
@@ -98,15 +124,44 @@ defmodule AuvalOffice.Policy do
   end
 
   @doc """
-  Define a rule
-  """
-  defmacro rule(id, actions, subject, object, options), do: do_rule(id, actions, subject, object, options)
-  defmacro rule(id, actions, subject, object, options, do: rule_block), do: do_rule(id, actions, subject, object, [do: rule_block] ++ options)
+  Define a rule.
 
-  def make_evaluator() do
+  Takes an additional `do` block, or a `do:` expression.
+
+  ### Parameters
+
+  * `id`: An identifier to identify and disambiguate the rule.
+  * `actions`: An action or list of action that is authorized (or forbidden) by this rule.
+    The action `:all` matches any action.
+    This is *not* a pattern.
+  * `subject`: A pattern matching the subject that this rule applies to.
+    If the pattern does not match, the rule is skipped.
+  * `object`: A pattern matching the object that this rule applies to.
+    If the pattern does not match, the rule is skipped.
+  * `options`: A keyword list of options as described below.
+
+  ### Options
+
+  * `action`: A pattern to bind the action to that is authorized.
+    Only necessary for advanced matching or if you need to refer to the action in the body of the rule.
+  * `context`: A pattern to match against the context of the authorization.
+  * `when`: A guard expression.
+    The rule is skipped if this expression evaluates to false.
+    Can refer to any variable bound by the patterns passed to this macro.
+    Will be evaluated in the context of an `if`, so not limited to guard expressions.
+  """
+  defmacro rule(id, actions, subject, object, options),
+    do: do_rule(id, actions, subject, object, options)
+
+  @doc false
+  defmacro rule(id, actions, subject, object, options, do: rule_block),
+    do: do_rule(id, actions, subject, object, [do: rule_block] ++ options)
+
+  defp make_evaluator() do
     quote do
       @rules_o Enum.reverse(@rules)
       @fetchers_o Enum.reverse(@fetchers)
+      @impl unquote(__MODULE__)
       def authorize(subject, object, action, context \\ %{}) do
         context =
           @fetchers_o
@@ -151,4 +206,35 @@ defmodule AuvalOffice.Policy do
   defmacro __before_compile__(_env) do
     make_evaluator()
   end
+
+  @doc """
+  Function generated in a module that `use`s this module.
+
+  ### Parameters
+
+  * `subject`: The subject that should be authorized.
+  * `object`: The object that would be acted upon by the subject.
+  * `action`: The action taken by the subject upen the object.
+  * `context`: A map of additional information necessary to make an authorization decision.
+    Defaults to the empty map `%{}` if not given.
+
+  ### Return value
+
+  * `{:ok, deciding_rule, parameters}`:
+
+    Returned when one rule returns a positive result.
+    The rule that made the decision is identified by the second element,
+    and any additional values returned by it are in the third element.
+
+  * `{:error, deciding_rule, parameters}`:
+
+    Returned when one rule returns a negative result.
+
+    The rule that made the decision is identified by the second element,
+    and any additional values returned by it are in the third element.
+
+    If no rule matched, the decision is considered negative,
+    and the second element will be `:default_deny`.
+  """
+  @callback authorize(subject :: term(), object :: term(), action :: atom(), context :: map()) :: authorization_result()
 end
